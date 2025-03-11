90d8827b9327a36b5dd02452fa9c9c38d20058c8
daniyal <dshamraiz61@gmail.com>
2025-03-11 15:50:05 +0500

    Initial commit

---
+++ assignment-1/arithmetic-operators.py
@@ -0,0 +1,17 @@
+                                         #Arithmetic Operators
+x=2
+y=4
+#Addition : add two numbers
+add=x+y  #output=8
+#Subtraction : subtracts two numbers
+sub=x-y  #output=-2
+#Multiplication : multiplies two numbers
+mul=x*y  #output=8
+#Division : divides and returns a float 
+div=x/y  #output=0.5
+#Floor Division : returns the integer quotient
+fdiv=x//y  #output=0
+#Modulus : returns the remainder
+mod=x%y  #output=2
+#Exponentiation : returns the remainder
+exp=x**y  #output=16
---
+++ assignment-1/assignment-operators.py
@@ -0,0 +1,17 @@
+                                         #Assignment Operators
+#Assign : assigns a value
+a = 10
+#Add & Assign : Adds and assigns
+a += 5
+#Subtract & Assign : Subtracts and assigns
+a -= 3
+#Multiply & Assign : Multiplies and assigns
+a *= 2
+#Divide & Assign : Divides and assigns	
+a /= 4
+#Floor Divide & Assign : Floor divides and assigns
+a //= 2
+#Modulus & Assign : Finds remainder and assigns
+a%= 2
+#Exponentiate & Assign : Raises power and assigns
+a **= 3
